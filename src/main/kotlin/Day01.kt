5b9a273379c4d8879c74feefcd6463b32ae02ee0
Maximilian Irro <maximilian.irro@viesure.io>
2021-12-01 23:35:06 +0100

    DAY-01 Better solution

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -5,9 +5,8 @@ class Day01 {
     private val measurements: List<Int> = readFileFromClasspath("day01_input.txt")
         .split("\n")
         .map { it.toInt() }
-
-    private fun sum(i: Int, j: Int): Int = measurements.subList(i, j).sum()
-
+    
+    /** Correct result is: 1624 */
     fun challenge1() {
         var prev = -1
         var count = 0
@@ -22,21 +21,27 @@ class Day01 {
         println("(challenge1) measurements that are larger than the previous measurement: $count")
     }
 
+    /** Correct result is: 1653 */
     fun challenge2() {
-        var count = 0
-        var i = 0
-
-        while (i + 3 < measurements.size) {
-
-            val sumA = sum(i, i+2)
-            val sumB = sum(i+1, i+3)
-
-            if (sumA < sumB) count += 1
-
-            i += 1
-        }
-
-        println("(challenge2) measurements that are larger than the previous measurement: $count")
+        println("(challenge2) measurements that are larger than the previous measurement: ${countWindows(measurements)}")
     }
+
+    private fun countWindows(ls: List<Int>): Int {
+
+        val a = ls.head3()
+        val b = ls.tail().head3()
+
+        return if (a.isEmpty() || b.isEmpty()) {
+          0
+        } else if (a.sum() < b.sum()) {
+            1 + countWindows(ls.tail())
+        } else {
+            countWindows(ls.tail())
+        }
+    }
+
+    private fun <T> List<T>.head3() = if (size >= 3) subList(0, 3) else emptyList()
+
+    private fun <T> List<T>.tail() = drop(1)
 
 }
