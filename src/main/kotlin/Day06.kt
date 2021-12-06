c61db2c5f981156d97951af7b4d674ee06f8e792
Maximilian Irro <max@irro.at>
2021-12-06 13:23:39 +0100

    DAY-06 Use tail recursion

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -4,27 +4,22 @@ class Day06(fileName: String): AdventDay(fileName) {
         .flatMap { it.split(",") }
         .map { it.toInt() }
 
-    override fun part1(): Long = EvolutionMap(initialFish).cycle(80)
+    override fun part1(): Long = LanternfishEvolution(initialFish).cycle(80)
 
-    override fun part2(): Long = EvolutionMap(initialFish).cycle(256)
+    override fun part2(): Long = LanternfishEvolution(initialFish).cycle(256)
 
     override fun printResult() {
         println("\n--- Day 6: Lanternfish ---\n")
-        println("Part 1: after 80 days the amount of lanternfish would be: ${part1()}")
-        println("Part 2: after 256 days the amount of lanternfish would be: ${part2()}")
+        println("Part 1: after 80 days the amount of Lanternfish would be: ${part1()}")
+        println("Part 2: after 256 days the amount of Lanternfish would be: ${part2()}")
     }
 
-    private class EvolutionMap(fish: List<Int>) {
-        private var timerMap: Map<Int, Long> = timerMap(fish)
+    private class LanternfishEvolution(private val fish: List<Int>) {
 
-        fun cycle(days: Int): Long {
-            for (day in 0 until days) {
-                tick()
-            }
-            return timerMap.values.sum()
-        }
+        fun cycle(days: Int): Long = tick(days, timerMap(fish)).values.sum()
 
-        private fun tick() {
+        private tailrec fun tick(day: Int, timerMap: Map<Int, Long>): Map<Int, Long> {
+            if (day == 0) return timerMap
             val updatedMap = mutableMapOf<Int, Long>()
             for (timer in 0..8) {
                 when (timer) {
@@ -33,7 +28,7 @@ class Day06(fileName: String): AdventDay(fileName) {
                     else -> updatedMap[timer] = timerMap.valueOrZero(timer + 1)
                 }
             }
-            timerMap = updatedMap
+            return tick(day-1, updatedMap)
         }
 
         private fun timerMap(fish: List<Int>): Map<Int, Long> = fish.groupingBy { it }.eachCount() as Map<Int, Long>
