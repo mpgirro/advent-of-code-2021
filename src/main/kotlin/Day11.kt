3da71fcbe83d596a647073815b4e16dc24244183
Maximilian Irro <max@irro.at>
2021-12-11 18:29:17 +0100

    DAY-11 Refactor

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -10,7 +10,20 @@ class Day11 : AdventDay(day = "day11") {
         println("Part 2: ${part2(input)}")
     }
 
-    private class Octopus(val x: Int, val y: Int, initialEnergy: Int) {
+    private fun grid(input: List<String>): Array<Array<Octopus>> {
+        val energy: Array<IntArray> = input
+            .map { it.toList() }
+            .map { it.map(Character::getNumericValue) }
+            .map { it.toIntArray() }
+            .toTypedArray()
+        return Array(10) { y ->
+            Array(10) { x ->
+                Octopus(x, y, energy[y][x])
+            }
+        }
+    }
+
+    private data class Octopus(val x: Int, val y: Int, private val initialEnergy: Int) {
         private var energyLevel = initialEnergy
         fun energyLevel(): Int = energyLevel
         fun increaseEnergy() = apply { energyLevel += 1 }
@@ -22,6 +35,8 @@ class Day11 : AdventDay(day = "day11") {
     }
 
     private class LightSimulation(private val grid: Array<Array<Octopus>>) {
+
+        private val allOctopuses: List<Octopus> =  grid.map { line -> line.toList() }.toList().flatten()
 
         fun simulateN(steps: Int): Int = if (steps > 0){
             simulateStep() + simulateN(steps-1)
@@ -41,19 +56,19 @@ class Day11 : AdventDay(day = "day11") {
         private fun simulateStep(): Int {
 
             // Step 1) Increase energy level of each octopus by 1
-            forEachOctopus { octopus ->
-                octopus.increaseEnergy()
+            forEachOctopus {
+                it.increaseEnergy()
             }
 
             // Step 2) Flash each octopus at most once if energy level is high enough
             val flashedOctopuses = mutableSetOf<Octopus>()
             forEachOctopus { octopus ->
                 if (octopus.isFlash() && octopus !in flashedOctopuses) {
-                    val remainingOctopuses = mutableStackOf(octopus)
                     flashedOctopuses.add(octopus)
+
+                    val remainingOctopuses = mutableStackOf(octopus)
                     while (remainingOctopuses.isNotEmpty()) {
-                        val nextOctopus = remainingOctopuses.pop()
-                        adjacent(nextOctopus)
+                        adjacent(remainingOctopuses.pop())
                             .filter { it !in flashedOctopuses }
                             .map { it.increaseEnergy() }
                             .filter { it.isFlash() }
@@ -66,35 +81,17 @@ class Day11 : AdventDay(day = "day11") {
             }
 
             // Step 3) Set energy level of all flashed octopuses to 0
-            forEachOctopus { octopus ->
-                if (octopus.isFlash()) {
-                    octopus.reset()
-                }
-            }
+            allOctopuses
+                .filter { it.isFlash() }
+                .forEach { it.reset() }
 
             // Report how many flashed
             return flashedOctopuses.size
         }
 
-        private fun forEachOctopus(block: (Octopus) -> Unit) {
-            for (y in 0 until 10) {
-                for (x in 0 until 10) {
-                    val octopus = grid[y][x]
-                    block(octopus)
-                }
-            }
-        }
+        private fun forEachOctopus(block: (Octopus) -> Unit) = allOctopuses.forEach(block)
 
-        fun <T> mapEachOctopus(block: (Octopus) -> T): List<T> {
-            val list = mutableListOf<T>()
-            for (y in 0 until 10) {
-                for (x in 0 until 10) {
-                    val octopus = grid[y][x]
-                    list.add(block(octopus))
-                }
-            }
-            return list
-        }
+        private fun <T> mapEachOctopus(block: (Octopus) -> T): List<T> = allOctopuses.map(block)
 
         private fun octopus(x: Int, y: Int): Octopus? {
             if (x < 0 || x > 9) return null
@@ -102,17 +99,19 @@ class Day11 : AdventDay(day = "day11") {
             return grid[y][x]
         }
 
-        private fun adjacent(octopus: Octopus): List<Octopus> =
-            listOfNotNull(
-                octopus(octopus.x - 1, octopus.y - 1),
-                octopus(octopus.x, octopus.y - 1),
-                octopus(octopus.x + 1, octopus.y - 1),
-                octopus(octopus.x - 1, octopus.y),
-                octopus(octopus.x + 1, octopus.y),
-                octopus(octopus.x - 1, octopus.y + 1),
-                octopus(octopus.x, octopus.y + 1),
-                octopus(octopus.x + 1, octopus.y + 1)
+        private fun adjacent(octopus: Octopus): List<Octopus> {
+            val (x,y) = octopus
+            return listOfNotNull(
+                octopus(x - 1, y - 1),
+                octopus(x, y - 1),
+                octopus(x + 1, y - 1),
+                octopus(x - 1, y),
+                octopus(x + 1, y),
+                octopus(x - 1, y + 1),
+                octopus(x, y + 1),
+                octopus(x + 1, y + 1)
             )
+        }
 
         private fun allFlash(): Boolean = mapEachOctopus { it.hasFlashed() }.all { it }
 
@@ -120,19 +119,6 @@ class Day11 : AdventDay(day = "day11") {
             grid.joinToString(separator = "\n") { line ->
                 line.joinToString(separator = "", transform = { "${it.energyLevel()}" })
             }
-    }
-
-    private fun grid(input: List<String>): Array<Array<Octopus>> {
-        val energy: Array<IntArray> = input
-            .map { it.toList() }
-            .map { it.map(Character::getNumericValue) }
-            .map { it.toIntArray() }
-            .toTypedArray()
-        return Array(10) { y ->
-            Array(10) { x ->
-                Octopus(x,y,energy[y][x])
-            }
-        }
     }
 
 }
